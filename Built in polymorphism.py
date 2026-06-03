d3c9e7ef0ae76b7583a7cc1d794b09a4731c6858
Jamuna20-14 <jamunaelumalai048@gmail.com>
2026-06-03 12:26:40 +0530

    Built in polymorphism.py

---
+++ Built in polymorphism.py
@@ -0,0 +1,44 @@
+print("len() Function")
+print(len("Python"))
+
+print("\nmax() Function")
+print(max(10, 20, 30))
+
+print("\nmin() Function")
+print(min([5, 8, 2, 9]))
+
+print("\nsum() Function")
+print(sum([10, 20, 30]))
+
+print("\n+ Operator")
+print(10 + 20)
+print("Hello " + "Python")
+print([1, 2] + [3, 4])
+
+print("\ntype() Function")
+print(type(100))
+print(type("Python"))
+print(type([1, 2, 3]))
+
+o/p:
+len() Function
+6
+
+max() Function
+30
+
+min() Function
+2
+
+sum() Function
+60
+
++ Operator
+30
+Hello Python
+[1, 2, 3, 4]
+
+type() Function
+<class 'int'>
+<class 'str'>
+<class 'list'>
